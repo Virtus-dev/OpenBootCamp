2b04ea14b6d0d6dc361ed68089977ecbe9c8a07a
virtus-dev <aritzgonzalez92@gmail.com>
2022-10-20 14:14:42 +0200

    ejercicio1

---
+++ project1/src/exercise1/Main.java
@@ -0,0 +1,18 @@
+package exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte    byte1 = 1;
+        short   short1 = 2;
+        int entero = 10;
+        float   flotante = 12f;
+        double  decimal = 13.12d;
+        char    caracter = 'a';
+        String  cadena = "Esto es una cadena";
+        boolean booleano = true;
+        System.out.println("byte: " + byte1 + " / short: " + short1 + " / int: " + entero + " / float: " + flotante + " / double: " + decimal + '\n');
+        System.out.println("char: " + caracter + " / String: " + cadena + '\n');
+        System.out.println("boolean: " + booleano);
+    }
+}
