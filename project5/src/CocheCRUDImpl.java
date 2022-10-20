062e8b8e05c68f215fc0002f2f03667bfe7f18cb
virtus-dev <aritzgonzalez92@gmail.com>
2022-10-20 17:58:08 +0200

    pject5 added

---
+++ project5/src/CocheCRUDImpl.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CocheCRUDImpl implements CocheCRUD
+{
+
+    @Override
+    public void save() {
+        System.out.println("metodo save() \n");
+
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("metodo findAll() \n");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("metodo delete()");
+    }
+}
